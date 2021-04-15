90545a5d22c46f3beda714b9408531108ee35e27
TristanL97 <tristanlossman@gmail.com>
2021-04-15 23:24:07 +0300

    Post in ASP NET Core REST API

--- BlazorTutorial/EmployeeManagement.Api/Controller/EmployeesController.cs
+++ BlazorTutorial/EmployeeManagement.Api/Controller/EmployeesController.cs
@@ -54,5 +54,26 @@ namespace EmployeeManagement.Api.Controllers
                     "Error retrieving data from the database");
             }
         }
+        
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            try
+            {
+                if(employee == null)
+                {
+                    return BadRequest();
+                }
+
+                var createdEmployee = await employeeRepository.AddEmployee(employee);
+
+                return CreatedAtAction(nameof(GetEmployee), new { id = createdEmployee.EmployeeId }, createdEmployee);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
     }
 }
